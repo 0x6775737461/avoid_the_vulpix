504f1b217bc321eeaf6ba76868e03cff3fbae821
0x6775737461 <fabiogustavo@tuta.io>
2022-01-21 10:19:37 -0400

    functions sketch

---
+++ src/install_minix.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# getting minix 3 iso file
+# get_minix_iso() {}
+
+# minix 3 have a hash to test?
+# file_validation() {}
+
+# create_img() {}
+# creating qemu img
+
+# minix 3 pre-installation, aka: first boot
+# pre_install() {}
+
+# mini 3 day2day use
+# start_minix3() {}
