0874d19a52159e2630cb165d5a725e9244f9cb66
0x6775737461 <fabiogustavo@tuta.io>
2022-01-21 10:51:28 -0400

    the basis of get_mini_iso function, implemented

--- src/install_minix.sh
+++ src/install_minix.sh
@@ -1,7 +1,20 @@
 #!/usr/bin/env bash
 
 # getting minix 3 iso file
-# get_minix_iso() {}
+get_minix_iso() {
+	#TODO: another function to get this data dinamically
+	local actual_version='R3.3.0-588a35b'
+	local md5_hash='3234ffcebfb2a28069cf3def41c95dec'
+
+	local minix_file='minix_version.iso.bz2'
+	local link='http://download.minix3.org/iso'
+
+	wget "${link}/${minix_file/version/${actual_version}}" \
+		-O 'minix3.iso'
+		--no-verbose \
+		--tries=3 \
+		--show-progress
+}
 
 # minix 3 have a hash to test?
 # file_validation() {}
@@ -14,3 +27,9 @@
 
 # mini 3 day2day use
 # start_minix3() {}
+
+main() {
+	get_minix_iso
+}
+
+main "$@"
